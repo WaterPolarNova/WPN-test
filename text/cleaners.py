437078b420af00de00548d5406868c4b31ede436
WaterPolarNova <117559585+WaterPolarNova@users.noreply.github.com>
2022-12-07 11:18:08 +0800

    Update cleaners.py

--- text/cleaners.py
+++ text/cleaners.py
@@ -446,6 +446,37 @@ def korean_cleaners(text):
   return text
 
 
+def japanese_phrase_cleaners(text):
+  '''Pipeline for dividing Japanese text into phrases.'''
+  sentences = re.split(_japanese_marks, text)
+  marks = re.findall(_japanese_marks, text)
+  text = ''
+  for i, sentence in enumerate(sentences):
+    if re.match(_japanese_characters, sentence):
+      if text != '':
+        text += ' '
+      labels = pyopenjtalk.extract_fullcontext(sentence)
+      for n, label in enumerate(labels):
+        phoneme = re.search(r'\-([^\+]*)\+', label).group(1)
+        if phoneme not in ['sil','pau']:
+          text += phoneme
+        else:
+          continue
+        a3 = int(re.search(r"\+(\d+)/", label).group(1))
+        if re.search(r'\-([^\+]*)\+', labels[n + 1]).group(1) in ['sil','pau']:
+          a2_next=-1
+        else:
+          a2_next = int(re.search(r"\+(\d+)\+", labels[n + 1]).group(1))
+        # Accent phrase boundary
+        if a3 == 1 and a2_next == 1:
+          text += ' '
+    if i<len(marks):
+      text += unidecode(marks[i]).replace(' ','')
+  if re.match('[A-Za-z]',text[-1]):
+    text += '.'
+  return text
+
+
 def chinese_cleaners(text):
   raise Exception("该笔记本已取消对于该cleaners的支持。你可以向其他相关领域人士继续寻求帮助。")
 
